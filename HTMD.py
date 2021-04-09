ddeae56c7d1fd457d332a04819774c229140a85d
ettore <tu@esempio.com>
2021-04-09 15:41:19 +0200

    Eliminato get_smile_formzinc, aggiunto rdkit

--- HTMD.py
+++ HTMD.py
@@ -8,7 +8,7 @@ from make_mdp import make_mdp
 from time import sleep
 import numpy as np 
 import matplotlib.pyplot as plt
-from utils import plot_xvg, send_mail, detachmet, get_zinc_smile
+from utils import plot_xvg, send_mail, detachmet
 from subprocess import Popen, PIPE
 import pandas as pd
 import pickle as pkl
