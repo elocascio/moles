b5fba076507741fb0a3556242c9fca2b8922e170
14802292 <beatrice.tropea@gmail.com>
2022-02-10 11:47:22 +0100

    update

--- protein.py
+++ protein.py
@@ -74,5 +74,5 @@ system(f'{args.mdrun} -v -deffnm MD -nt {args.numthread} -gpu_id {args.deviceID}
 
 #------------- trj
 if args.trj:
-	system(f'echo \'Protein\nSystem\n\' | {args.gmx} trjconv -s MD.tpr -f MD.xtc -o trj.xtc -pbc nojump -center')
+	system(f'echo \'System\' | {args.gmx} trjconv -s MD.tpr -f MD.xtc -o trjadj.xtc -pbc mol -ur compact')
 	deviceID = gpu_manager()
