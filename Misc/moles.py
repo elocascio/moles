8c6ce879915d634fb4ee489547ab717b46964ee4
Ekein <ettore.locascio@unicatt.it>
2022-02-18 12:42:11 +0100

    bar fixed

--- Misc/moles.py
+++ Misc/moles.py
@@ -2,7 +2,8 @@ import time
 from random import randint
 
 def init():
-  if randint(0,1) == 0:
+  rand = randint(0,2)
+  if rand == 0:
     print("""
                                88             
                                88             
@@ -36,10 +37,10 @@ MOLEcular dynamics Suite
                           `::.  \   `::. !'                                
                            _.`::.\     ::                                  
                         .-~_____:~~    :'                                  
-                        ~~~  .-'__..-~'           Loki                         
+                        ~~~  .-'__..-~'         THE MOLES TEAM                         
                              ~~~
 """)
-  else:
+  elif rand == 1:
     print("""
                                88             
                                88             
@@ -70,7 +71,44 @@ MOLEcular dynamics Suite
                               ;,' ';
                              \|/\|/;\|/           Pushi          
 """)
+  elif rand == 2:
+    print("""
+                               88             
+                               88             
+                               88             
+88,dPYba,,adPYba,   ,adPPYba,  88  ,adPPYba, ,adPPYba,  
+88P'   "88"    "8a a8"     "8a 88 a8P_____88 I8[    "" 
+88      88      88 8b       d8 88 8PP\"\"\"\"\"\"\"  `'Y8ba,
+88      88      88 "8a,   ,a8" 88 "8b,   ,aa aa    ]8I
+88      88      88  `"YbbdP"'  88  `"Ybbd8"' `"YbbdP"'
 
+MOLEcular dynamics Suite
+
+           /|       |\\
+        `__\\       //__'
+           ||      ||
+         \__`\     |'__/
+           `_\\   //_'
+           _.,:---;,._
+           \_:     :_/
+             |@. .@|
+             |     |
+             ,\.-./ \\
+             ;;`-'   `---__________-----.-.
+             ;;;                         \_\\
+             ';;;                         |
+              ;    |                      ;
+               \   \     \        |      /
+                \_, \    /        \     |\\
+                  |';|  |,,,,,,,,/ \    \ \_
+                  |  |  |           \   /   |
+                  \  \  |           |  / \  |
+                   | || |           | |   | |
+                   | || |           | |   | |
+                   | || |           | |   | |
+                   |_||_|           |_|   |_|
+                  /_//_/           /_/   /_/      Loki
+""")
     time.sleep(1.01)
 
 if __name__=='__main__':
