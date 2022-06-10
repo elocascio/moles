a18a852a514535496ab68505585f0e9eb47bb825
Ettore Lo Cascio <ettore.locascio@unicatt.it>
2022-06-10 08:04:52 +0000

    Update plip_contacts.py

--- plip_contacts.py
+++ plip_contacts.py
@@ -60,11 +60,11 @@ def contacts(pdb = 'MD.pdb', xtc = 'MD.xtc', step = 10, ligand = 'UNK'):
 ##################################### dictionaries #########################################################
     res_list = []
     if ion != None:
-        for numres, nameres in list(zip(comp_ion.resnums, comp_ion.resnames)): res_list.append(str(numres)+ str(nameres)):
-            dic = {key : [] for key in res_list}
+        for numres, nameres in list(zip(comp_ion.resnums, comp_ion.resnames)): res_list.append(str(numres)+ str(nameres))
+        dic = {key : [] for key in res_list}
     else:
-        for numres, nameres in list(zip(complexo.resnums, complexo.resnames)): res_list.append(str(numres)+ str(nameres)):
-            dic = {key : [] for key in res_list}
+        for numres, nameres in list(zip(complexo.resnums, complexo.resnames)): res_list.append(str(numres)+ str(nameres))
+        dic = {key : [] for key in res_list}
     Hydrophobic_dic, PiStacking_dic, PiCation_dic, HBond_dic, SaltBridge_dic = [dic] * 5
 ###########################################################################################################
     unk = u.select_atoms(f'resname {ligand}'); lig_id = unk.resids[0]; lig_chain = unk.chainIDs[0]
