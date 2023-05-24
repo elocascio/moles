c94be552ad7cd487b9fbaa4255c98dbacb130dc3
Ettore Lo Cascio <ettore.locascio@unicatt.it>
2023-05-24 10:03:16 +0000

    Update file moles.py

--- Misc/moles.py
+++ Misc/moles.py
@@ -2,7 +2,7 @@ import time
 from random import randint
 
 def init():
-  rand = randint(0,2)
+  rand = randint(0,1)
   if rand == 0:
     print("""
                                88             
@@ -41,37 +41,6 @@ MOLEcular dynamics Suite
                              ~~~
 """)
   elif rand == 1:
-    print("""
-                               88             
-                               88             
-                               88             
-88,dPYba,,adPYba,   ,adPPYba,  88  ,adPPYba, ,adPPYba,  
-88P'   "88"    "8a a8"     "8a 88 a8P_____88 I8[    "" 
-88      88      88 8b       d8 88 8PP\"\"\"\"\"\"\"  `'Y8ba,
-88      88      88 "8a,   ,a8" 88 "8b,   ,aa aa    ]8I
-88      88      88  `"YbbdP"'  88  `"Ybbd8"' `"YbbdP"'
-
-MOLEcular dynamics Suite
-
-                              ,`""',
-                              ;' ` ;
-                              ;`,',;
-                              ;' ` ;
-                         ,,,  ;`,',;
-                        ;,` ; ;' ` ;   ,',
-                        ;`,'; ;`,',;  ;,' ;
-                        ;',`; ;` ' ; ;`'`';
-                        ;` '',''` `,',`',;
-                         `''`'; ', ;`'`'
-                              ;' `';
-                              ;` ' ;
-                              ;' `';
-                              ;` ' ;
-                              ; ',';
-                              ;,' ';
-                             \|/\|/;\|/           Pushi          
-""")
-  elif rand == 2:
     print("""
                                88             
                                88             
